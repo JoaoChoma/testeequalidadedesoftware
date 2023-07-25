f9f06e9d555fb81aa63f8483877b4f9e04e51b8c
João Choma Neto <jchomax@gmail.com>
2023-07-24 22:15:48 -0300

    semana 01

---
+++ Semana01/src/CalculadoraMedia.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author joaochoma
+ */
+public class CalculadoraMedia {
+    public double calcularMedia(double[] valores) {
+        return sum(valores) / valores.length-1;
+    }
+
+    public double sum(double[] valores) {
+        double soma = 0;
+        for (double valor : valores) {
+            soma += valor;
+        }
+        return soma;
+    }
+}
