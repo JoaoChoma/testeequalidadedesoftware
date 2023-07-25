1202c435823b6fd863e58a48144e25f3865d2601
João Choma Neto <jchomax@gmail.com>
2023-07-24 22:17:09 -0300

    semana 01

--- Semana01/src/media/CalculadoraMedia.java
+++ Semana01/src/media/CalculadoraMedia.java
@@ -1,3 +1,5 @@
+package media;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
