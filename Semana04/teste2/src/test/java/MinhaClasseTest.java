6499ae979918aa32ed4bb8341d0728eb1f9cce84
João Choma Neto <jchomax@gmail.com>
2023-08-13 00:33:50 -0300

    tentativa 1

---
+++ Semana04/teste2/src/test/java/MinhaClasseTest.java
@@ -0,0 +1,5 @@
+import org.junit.Test;
+
+@Test
+public class MinhaClasseTest {
+}
---
+++ Semana04/teste2/src/test/java/MinhaClasseTestTest.java
@@ -0,0 +1,7 @@
+import junit.framework.TestCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinhaClasseTestTest extends TestCase {
+
+}
